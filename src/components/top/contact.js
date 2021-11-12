88bd54c7a7a6db9a588061ed150a4ad40051149f
Chusa212 <chese2001@gmail.com>
2021-11-13 00:56:59 +0900

    up

--- src/components/top/contact.js
+++ src/components/top/contact.js
@@ -1,10 +1,44 @@
 import React from "react";
+import {Googleform} from './googleformContact'
 import "../../styles/top_style/contact.css"
 
 const Contact = () => {
   return (
     <>
-      <p>contact</p>
+       <form>
+      <div className='StyledTable'>
+        <tbody>
+          <tr>
+            <Th>
+              <p>Name</p>
+            </Th>
+            <td>
+              <div inputtype={'text'} name={'name'} />
+            </td>
+          </tr>
+          <tr>
+            <Th>
+            <p>Email</p>
+            </Th>
+            <td> 
+              <div inputtype='text'} name={'email'} />
+            </td>
+          </tr>
+          <tr>
+            <Th>
+            <p>subject</p>
+            </Th>
+            <td> 
+              <StyledInput type={'text'} name={'subject'} />
+            </td>
+          </tr>
+        </tbody>
+      </div>
+      <p>お問い合わせ内容</p>
+      <TextArea name={'inquiry'} />
+      <StyledSubmit type={'submit'}>送信する</StyledSubmit>
+    </form>
+     
     </>
   )
 }
