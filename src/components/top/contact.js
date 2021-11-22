7553578782339b78a75ca7be6ac80d1a38064d50
Chusa212 <chese2001@gmail.com>
2021-11-23 00:50:24 +0900

    up

--- src/components/top/contact.js
+++ src/components/top/contact.js
@@ -1,54 +1,36 @@
 import React from "react";
 // import {Googleform} from './googleformContact'
-import "../../styles/top_style/contact.css"
+import "../../styles/top_style/contact.css";
 
 // const StyledTable='styled.table';
 // const TextArea = 'styled.textarea';
 // const Th = 'styled.th';
 // const StyledSubmit ='styled.button';
 
-
 const Contact = () => {
   return (
     <>
-  {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeaB9Nt2pdWD4MNtKFh519nCCh8HBUQu1xt0H5PMH1QrvCkRA/viewform?embedded=true" width="640" height="812" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe> */}
-{/*   
-       <form>
-      <StyleTable className='StyledTable'>
-        <tbody>
-          <tr>
-            <Th>
-              <p>Name</p>
-            </Th>
-            <td>
-              <div inputtype={'text'} name={'name'} />
-            </td>
-          </tr>
-          <tr>
-          <Th>
-            <p>Email</p>
-           </Th>
-            <td> 
-              <div inputtype={'text'} name={'email'} />
-            </td>
-          </tr>
-          <tr>
-            <Th>
-            <p>subject</p>
-            </Th>
-            <td> 
-              <StyledInput type={'text'} name={'subject'} />
-            </td>
-          </tr>
-        </tbody>
-      </StyleTable>
-      <p>Contact</p>
-      <TextArea name={'inquiry'} />
-      <StyledSubmit type={'submit'}>送信する</StyledSubmit>
-    </form>
-      */}
+      <form>
+        <label>
+          Name:
+          <input type="text" name="Name" />
+        </label>
+        <label>
+        Email:
+          <input type="text" name="Email" />
+        </label>
+        <label>
+        Subject:
+          <input type="text" name="Subject" />
+        </label>
+        <label>
+        Messages:
+          <input type="text area" name="Messages" />
+        </label>
+        <input type="submit" value="送信" />
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
